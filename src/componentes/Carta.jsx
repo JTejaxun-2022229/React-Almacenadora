0a407aaa00bd053f04f6a5b36919623ac14b72ab
Jose Tejaxun <jtejaxun-2022229@kinal.edu.gt>
2024-05-03 23:41:49 -0600

    se aregla visibilidad de cards

--- src/componentes/Carta.jsx
+++ src/componentes/Carta.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { EditTask } from '../metodos/EditarTarea';
 import '../styles/card.css';
- 
+
 export const Card = ({ taskObj, index, deleteTask, updateListArray }) => {
     const [modal, setModal] = useState(false);
- 
+
     const colors = [
         { primaryColor: "#5D93E1", secondaryColor: "#ECF3FC" },
         { primaryColor: "#F9D288", secondaryColor: "#FEFAF1" },
@@ -12,20 +12,29 @@ export const Card = ({ taskObj, index, deleteTask, updateListArray }) => {
         { primaryColor: "#F48687", secondaryColor: "#FDF1F1" },
         { primaryColor: "#B964F7", secondaryColor: "#F3F0FD" }
     ];
- 
+
     const toggle = () => setModal(!modal);
-   
+
     const updateTask = (obj) => updateListArray(obj, index);
- 
+
     const handleDelete = () => deleteTask(index);
- 
+
+    // Formatear la fecha de inicio
+    const formattedStartDate = taskObj.startDate ? new Date(taskObj.startDate).toLocaleDateString() : "";
+
+    // Formatear la fecha de vencimiento
+    const formattedDueDate = taskObj.DueDate ? new Date(taskObj.DueDate).toLocaleDateString() : "";
+
     return (
         <div className="custom-card">
             <div className="custom-card-header" style={{ backgroundColor: colors[index % 5].primaryColor }}>{taskObj.Name}</div>
-            <div className="custom-card-header" style={{ backgroundColor: colors[index % 5].primaryColor }}>{taskObj.dueDate}</div>
-            <div className="custom-card-header" style={{ backgroundColor: colors[index % 5].primaryColor }}>{taskObj.status}</div>
             <div className="custom-card-body">
                 <p>{taskObj.Description}</p>
+                <p>Author: {taskObj.Author}</p>
+                <p>Start Date: {formattedStartDate}</p>
+                <p>Due Date: {formattedDueDate}</p>
+                <p>Status: {taskObj.Status}</p>
+                <br></br>
                 <div className="custom-card-actions">
                     <button className="edit-btn" onClick={() => setModal(true)}>Editar</button>
                     <button className="delete-btn" onClick={handleDelete}>Eliminar</button>
